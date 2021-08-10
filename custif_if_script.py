9bb0d6062b9bad6ca174a6354c780fe94e86949f
Dalissa <dalissa-lb@users.noreply.github.com>
2021-08-10 22:29:28 +0000

    Custom Script

---
+++ custif_if_script.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+genre = input("What type of manga would you like to read?")
+
+
+if genre == "romance"
+    print("We recommend reading Akatsuki No Yona")
+
+if genre == "psychological"
+    print("We recommend reading Psycho Pass")
+
+if genre == "violent"
+    print("We recommend reading Attack on Titan")
