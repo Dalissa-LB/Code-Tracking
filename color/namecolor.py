5f7f33ce1e18a3ec713e5c9b9da703bef4adfb21
Dalissa <dalissa-lb@users.noreply.github.com>
2021-08-11 22:48:23 +0000

    Playing with python pip packages

---
+++ color/namecolor.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Alta3 Research || Author RZFeeser@alta3.com
+Learning how to use functions"""
+
+## Installs the crayons package.
+## python3 -m pip install crayons
+## import statements ALWAYS go up top
+import crayons
+
+def main():
+    """Runtime code. Always indent a function"""
+
+    print(crayons.red('Dalissa'))
+
+    print(crayons.blue('Gabrielle'))
+
+    print(crayons.magenta('Spencer'))
+
+
+# we must call our main function or our code will not run!
+main()
+
--- cust_if_script.py
+++ cust_if_script.py
@@ -3,13 +3,13 @@
 genre = input("What type of manga would you like to read?")
 
 
-if genre == "romance"
+if genre = romance:
     message = 'We recommend reading Akatsuki No Yona'
 
-if genre == "psychological"
+if genre = psychological:
     message = 'We recommend reading Psycho Pass'
 
-if genre == "violent"
+if genre = violent:
     message = 'We recommend reading Attack on Titan'
 
 else:
